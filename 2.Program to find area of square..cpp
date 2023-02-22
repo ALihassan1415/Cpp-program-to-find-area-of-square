781d37757c52acf61a1fefc1910bc5c26fbe798e
Ali Hassan <125749746+ALihassan1415@users.noreply.github.com>
2023-02-22 09:35:17 +0500

    Add files via upload

---
+++ 2.Program to find area of square..cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+	int a;
+	float area;
+	cout<<"Enter the value of one side : ";
+	cin>>a;
+	area=a*a;
+	cout<<"The area of a Square is : "<<endl<<area<<" square metre";
+	return 0;
+}
